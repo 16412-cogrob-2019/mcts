d077c4b8b4c068818c1eb85e71e14ffb130206c5
Can Pu <pucanmail@gmail.com>
2019-05-04 02:47:25 -0400

    Transformed to python2

--- src/mcts.py
+++ src/mcts.py
@@ -4,7 +4,8 @@ from state import AbstractState, AbstractAction
 
 
 class Node(object):
-    def __init__(self, state: AbstractState):
+    def __init__(self, state):
+        # type: (AbstractState) -> None
         """ Create a Node object with given state
         """
         self._state = state
@@ -15,7 +16,8 @@ class Node(object):
         self.num_samples = 0
 
     @property
-    def state(self) -> AbstractState:
+    def state(self):
+        # type: () -> AbstractState
         return self._state
 
     @property
@@ -23,11 +25,13 @@ class Node(object):
         return self._state.is_terminal
 
     @property
-    def parent(self) -> "Node":
+    def parent(self):
+        # type: () -> Node
         return self._parent
 
     @property
-    def depth(self) -> int:
+    def depth(self):
+        # type: () -> int
         """ The depth (distance to the root, whose parent is None, plus 1) of
             the node
         """
@@ -39,16 +43,19 @@ class Node(object):
         return depth
 
     @property
-    def unused_edges(self) -> list:
+    def unused_edges(self):
+        # type: () -> list
         return self._untried_edges
 
     @property
-    def is_expanded(self) -> bool:
+    def is_expanded(self):
+        # type: () -> bool
         """ Whether all possible actions have been tried
         """
         return len(self._untried_edges) == 0
 
-    def add_child(self, action: AbstractAction) -> "Node":
+    def add_child(self, action):
+        # type: (AbstractAction) -> Node
         """ Add a child node and set the parent of the child node and return the
             child node
         :param action: The action that would lead the current node to the child
@@ -62,7 +69,8 @@ class Node(object):
         child._parent = self
         return child
 
-    def remove_child(self, child: "Node") -> "Node":
+    def remove_child(self, child):
+        # type: (Node) -> Node
         """ Remove a child node from this node
         :param child: The child node
         """
@@ -77,15 +85,18 @@ class Node(object):
             raise ValueError("The node does not have the given child node")
         return self
 
-    def __eq__(self, other: "Node") -> bool:
+    def __eq__(self, other):
+        # type: (Node) -> bool
         return (self.__class__ == other.__class__ and
                 self._state == other._state and self._parent == other._parent)
 
-    def __str__(self) -> str:
+    def __str__(self):
+        # type: () -> str
         return str(self._state)
 
 
-def select(node: Node, exploration_const: float = 1.0) -> (AbstractAction, Node):
+def select(node, exploration_const=1.0):
+    # type: (Node, float) -> (AbstractAction, Node)
     """ Select the best child node based on UCB; if there are multiple
         child nodes with the max UCB, randomly select one
     :param node: The parent node
@@ -108,7 +119,8 @@ def select(node: Node, exploration_const: float = 1.0) -> (AbstractAction, Node)
     return max_action, node.children[max_action]
 
 
-def expand(node: Node) -> Node:
+def expand(node):
+    # type: (Node) -> Node
     """ Randomly select an untried action and create a child node based on it
         Return the new child node
     :param node: The parent node
@@ -121,7 +133,8 @@ def expand(node: Node) -> Node:
     return node.add_child(action)
 
 
-def default_rollout_policy(state: AbstractState) -> float:
+def default_rollout_policy(state):
+    # type: (AbstractState) -> float
     """ The default policy for simulation is to randomly (uniform distribution)
         select an action to update the state and repeat the simulation until
         a terminal state is reached
@@ -134,7 +147,8 @@ def default_rollout_policy(state: AbstractState) -> float:
     return state.reward
 
 
-def backpropagate(node: Node, reward: float = 0.0) -> None:
+def backpropagate(node, reward=0.0):
+    # type: (Node, float) -> None
     """ Propagate the reward and sample count from the specified leaf node
         back all the way to the root node (the node with no parent)
     :param node: The node where the reward starts
@@ -146,10 +160,11 @@ def backpropagate(node: Node, reward: float = 0.0) -> None:
         node = node.parent
 
 
-def execute_round(root: Node, max_tree_depth: int = 15,
+def execute_round(root, max_tree_depth=15,
                   tree_select_policy=select, tree_expand_policy=expand,
                   rollout_policy=default_rollout_policy,
-                  backpropagate_method=backpropagate) -> None:
+                  backpropagate_method=backpropagate):
+    # type: (Node, int, callable, callable, callable, callable) -> None
     """ Perform selection, expansion, simulation and backpropagation with
         one sample
         :param root: The Node object from which the select step starts
@@ -182,11 +197,11 @@ def execute_round(root: Node, max_tree_depth: int = 15,
 
 
 class MonteCarloSearchTree:
-    def __init__(self, initial_state: AbstractState, samples: int = 1000,
-                 max_tree_depth: int = 10,
+    def __init__(self, initial_state, samples=1000, max_tree_depth=10,
                  tree_select_policy=select, tree_expand_policy=expand,
                  rollout_policy=default_rollout_policy,
                  backpropagate_method=backpropagate):
+        # type: (AbstractState, int, int, callable, callable, callable, callable) -> None
         """ Create a MonteCarloSearchTree object
         :param initial_state: The initial state
         :param samples: The number of samples to generate to obtain the best
@@ -218,7 +233,8 @@ class MonteCarloSearchTree:
         self._root = Node(initial_state)
         self._max_tree_depth = max_tree_depth
 
-    def _search(self, node: Node, search_depth: int = 1) -> (float, list):
+    def _search(self, node, search_depth=1):
+        # type: (Node, int) -> (float, list)
         """ Recursively search for a best sequence of actions
             :return: The reward of the last (deepest child node) and the
                      sequence of actions that leads to the optimal child node
@@ -247,8 +263,8 @@ class MonteCarloSearchTree:
                 best_reward = child_reward
         return best_reward, best_act_seq
 
-    def search_for_actions(self, search_depth: int = 1,
-                           random_seed: int = None) -> list:
+    def search_for_actions(self, search_depth=1, random_seed=None):
+        # type: (int, int) -> list
         """ With given initial state, obtain the best actions to take by MCTS
         :param search_depth: How many steps of actions are wanted
         :param random_seed: When not None, set the random seed before running
@@ -265,7 +281,8 @@ class MonteCarloSearchTree:
                           backpropagate_method=self._back_propagate_policy)
         return self._search(self._root, search_depth)[1]
 
-    def update_root(self, action: AbstractAction) -> "MonteCarloSearchTree":
+    def update_root(self, action):
+        # type: (AbstractAction) -> MonteCarloSearchTree
         """ Update the root node to reflect the new state after an action is
             taken
         :param action: The action that brings a new state
--- src/state.py
+++ src/state.py
@@ -1,22 +1,27 @@
+from abc import ABC
 import networkx as nx
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 
-class AbstractAction:
+class AbstractAction(ABC):
     pass
 
 
-class AbstractState:
+class AbstractState(ABC):
     @property
-    def reward(self) -> float:
+    def reward(self):
+        # type: () -> float
         raise NotImplementedError("The method not implemented")
 
     @property
-    def is_terminal(self) -> bool:
+    def is_terminal(self):
+        # type: () -> bool
         raise NotImplementedError("The method not implemented")
 
     @property
-    def possible_actions(self) -> list:
+    def possible_actions(self):
+        # type: () -> list
         """ The possible actions to take at this state
             Make sure that the returned list is not empty unless the state is
             a terminal state
@@ -24,41 +29,53 @@ class AbstractState:
         """
         raise NotImplementedError("The method not implemented")
 
-    def execute_action(self, action: AbstractAction) -> "AbstractState":
+    def execute_action(self, action):
+        # type: (AbstractAction) -> AbstractState
+        """ Execute the specified action on a copy of the current state
+        :param action
+        :return: The copy of the updated state
+        """
         raise NotImplementedError("The method not implemented")
 
 
 class KolumboAction(AbstractAction):
-    def __init__(self, agent_id: int, start_loc: int, end_loc: int,
-                 time_duration: float):
+    def __init__(self, agent_id, start_loc, end_loc, time_duration):
+        # type: (int, int, int, float) -> None
         self._agent_id = agent_id
         self._start_location = start_loc
         self._end_location = end_loc
         self._time = time_duration
 
     @property
-    def agent_index(self) -> int:
+    def agent_index(self):
+        # type: () -> int
         return self._agent_id
 
     @property
-    def start_location(self) -> int:
+    def start_location(self):
+        # type: () -> int
         return self._start_location
 
     @property
-    def goal_location(self) -> int:
+    def goal_location(self):
+        # type: () -> int
         return self._end_location
 
     @property
-    def time_duration(self) -> float:
+    def time_duration(self):
+        # type: () -> float
         return self._time
 
 
 class KolumboState(AbstractState):
-    def __init__(self, environment: nx.DiGraph = nx.DiGraph(),
-                 time_remains: float = 10.0, recovery_required: bool = False):
-        """ Create a state of the Koloumb volcano exploration mission
+    def __init__(self, environment=nx.DiGraph(), time_remains=10.0):
+        # type: (nx.DiGraph, float) -> None
+        """ Create a state of the Kolumbo volcano exploration mission
+            statuses[agent_id] describes the moving condition of an agent in the
+            format (start_node, end_node, time_remaining_to_get_to_end_node);
+            when start_node and end_node are the same, the agent is exactly at
+            that node
         """
-        # TODO: Add the planned path in
         # TODO: Add interfaces for ROS
         if time_remains < 0:
             raise ValueError("The remaining time cannot be negative")
@@ -68,9 +85,9 @@ class KolumboState(AbstractState):
         self._environment = environment
         self._time_remains = time_remains
         self._agent_id = 0  # The index for the agent that should take action
-        self._recovery_required = recovery_required
 
-    def __copy__(self) -> "KolumboState":
+    def __copy__(self):
+        # type: () -> KolumboState
         """ Make a copy of the state; histories, statuses and terminal_locations
             are copied by value
         """
@@ -81,87 +98,133 @@ class KolumboState(AbstractState):
         new_state._terminal_locations = self._terminal_locations.copy()
         return new_state
 
-    def add_location(self, location_id: int, reward: float) -> "KolumboState":
-        """ Add a location with the specified reward
+    def add_location(self, location_id, reward, coord):
+        # type: (int, float, (float, float)) -> KolumboState
+        """ Add a location with the specified reward and coordinates
         """
-        self._environment.add_node(location_id, reward=reward)
+        self._environment.add_node(location_id, reward=reward, coord=coord)
         return self
 
-    def remove_location(self, location_id: int) -> "KolumboState":
+    def remove_location(self, location_id):
+        # type: (int) -> KolumboState
         """ Remove a node and all adjacent edges
         """
         self._environment.remove_node(location_id)
         return self
 
-    def set_reward(self, location_id: int, reward: float) -> "KolumboState":
+    def set_location_reward(self, location_id, reward):
+        # type: (int, float) -> KolumboState
         """ Update the reward at a specified node
         """
-        nx.set_node_attributes(self._environment, {location_id: {
-            'reward': reward}})
+        nx.set_node_attributes(self._environment, name='reward',
+                               values={location_id: reward})
         return self
 
-    def reset_environment(self) -> "KolumboState":
-        """ Clear all locations and paths in the environment
+    def set_location_coord(self, location_id, coord):
+        # type: (int, (float, float)) -> KolumboState
+        """ Update the coordinates at a specified node
+        """
+        nx.set_node_attributes(self._environment, name='coord',
+                               values={location_id: coord})
+        return self
+
+    def reset_environment(self):
+        # type: () -> KolumboState
+        """ Clear all rewards_at_all_locations and costs_at_all_paths in the environment
         """
         self._environment = nx.DiGraph()
         return self
 
     @property
-    def locations(self) -> dict:
-        """ All possible locations and reward in the format
+    def rewards_at_all_locations(self):
+        # type: () -> dict
+        """ All possible locations and rewards in the format
             {location_id: reward}
         """
         return nx.get_node_attributes(self._environment, 'reward')
 
-    def reward_at_location(self, location_id: int) -> float:
+    def reward_at_location(self, location_id):
+        # type: (int) -> float
         """ The reward at the specified location
         """
         return nx.get_node_attributes(self._environment, 'reward')[location_id]
 
-    def add_path(self, start_location: int, end_location: int, cost: float) \
-            -> "KolumboState":
-        """ Add a path from start_location to end_location with the specified
-            cost
+    @property
+    def coord_at_all_locations(self):
+        # type: () -> dict
+        """ All possible locations and coordinates in the format
+            {location_id: coord}
         """
-        self._environment.add_edge(start_location, end_location, cost=cost)
+        return nx.get_node_attributes(self._environment, 'coord')
+
+    def add_path(self, start_location, end_location, cost, trajectory=None):
+        # type: (int, int, float, list) -> KolumboState
+        """ Add a path from start_location to end_location with the specified
+            cost and path (if existing)
+        """
+        if trajectory:
+            self._environment.add_edge(start_location, end_location, cost=cost,
+                                       trajectory=trajectory)
+        else:
+            self._environment.add_edge(start_location, end_location, cost=cost)
         return self
 
-    def remove_path(self, start_location: int, end_location: int) \
-            -> "KolumboState":
+    def remove_path(self, start_location, end_location):
+        # type: (int, int) -> KolumboState
         """ Remove a path from start_location to end_location
         """
         self._environment.remove_edge(start_location, end_location)
         return self
 
-    def set_cost(self, start_location: int, end_location: int, cost: float) \
-            -> "KolumboState":
+    def set_cost(self, start_location, end_location, cost):
+        # type: (int, int, float) -> KolumboState
         """ Update the cost at a specified path
         """
-        nx.set_edge_attributes(self._environment,
-                               {(start_location, end_location): {'cost': cost}})
+        nx.set_edge_attributes(self._environment, name='cost',
+                               values={(start_location, end_location): cost})
+        return self
+
+    def set_trajectory(self, start_location, end_location, trajectory):
+        # type: (int, int, list) -> KolumboState
+        """ Update the trajectory of a specified path
+        """
+        nx.set_edge_attributes(self._environment, name='trajectory', values={
+            (start_location, end_location): trajectory})
         return self
 
     @property
-    def paths(self) -> dict:
-        """ All possible paths and cost in the format
+    def costs_at_all_paths(self):
+        # type: () -> dict
+        """ All possible paths and costs in the format
             {(start_id, end_id): cost}
         """
         return nx.get_edge_attributes(self._environment, 'cost')
 
-    def cost_at_path(self, start_location: int, end_location: int) -> float:
+    def cost_at_path(self, start_location, end_location):
+        # type: (int, int) -> float
         """ The cost of a specified path
         """
         return nx.get_edge_attributes(
             self._environment, 'cost')[(start_location, end_location)]
 
-    def outgoing_paths(self, location_id: int) -> dict:
+    @property
+    def trajectories_at_all_paths(self):
+        # type: () -> dict
+        """ All possible paths and trajectories in the format
+            {(start_id, end_id): trajectory}
+        """
+        return nx.get_edge_attributes(self._environment, 'trajectory')
+
+    def outgoing_paths(self, location_id):
+        # type: (int) -> dict
         """ Locations that can be reached from the specified location with a
             single-step action in the format {(location_id, end_location): cost}
         """
         return {(tup[0], tup[1]): tup[2]['cost'] for tup in
                 self._environment.out_edges([location_id], data=True)}
 
-    def add_agent(self, location_id: int) -> "KolumboState":
+    def add_agent(self, location_id):
+        # type: (int) -> KolumboState
         """ Add an agent at the specified location
         """
         self._histories.append([location_id])
@@ -169,7 +232,8 @@ class KolumboState(AbstractState):
         return self
 
     @property
-    def nonterminal_agents(self) -> list:
+    def nonterminal_agents(self):
+        # type: () -> list
         """ The list of agents that can still move; if time runs out, then no
             agent can move; if an agent reaches a terminal location, then it
             can no longer move
@@ -180,8 +244,9 @@ class KolumboState(AbstractState):
                 self._statuses[index][0] not in
                 self._terminal_locations]
 
-    def evolve(self) -> "KolumboState":
-        """ Evolve the state so that one agent finished the ongoing action
+    def evolve(self):
+        # type: () -> KolumboState
+        """ Evolve the state so that one agent finishes the ongoing action
             Update the time remaining, histories, statuses, and the index of the
             agent that should take the next action
         """
@@ -203,16 +268,18 @@ class KolumboState(AbstractState):
         return self
 
     @property
-    def visited(self) -> set:
-        """ The set of all visited locations
+    def visited(self):
+        # type: () -> set
+        """ The set of all visited rewards_at_all_locations
         """
         return set(location for history in self._histories
                    for location in history)
 
     @property
-    def is_recovered(self) -> bool:
+    def is_recovered(self):
+        # type: () -> bool
         """ Whether all agents have reached a terminal location (if required)
-            Return True when recovery is not required
+            Return True when recovery is not required (no terminal locations)
         """
         if self._terminal_locations:
             return all(self._statuses[robot][0] in self._terminal_locations
@@ -221,18 +288,21 @@ class KolumboState(AbstractState):
             return True
 
     @property
-    def reward(self) -> float:
+    def reward(self):
+        # type: () -> float
         """ The reward at a terminal state
             If a state is not terminal, or if not all robots are recovered
             while required, return 0 reward
         """
         if not self.is_terminal or not self.is_recovered:
             return 0.0
-        return sum(reward for loc, reward in self.locations.items() if
-                   loc in self.visited)
+        return sum(
+            reward for loc, reward in self.rewards_at_all_locations.items() if
+            loc in self.visited)
 
     @property
-    def is_terminal(self) -> bool:
+    def is_terminal(self):
+        # type: () -> bool
         """ Whether a state is terminal
             If time runs out, then a state is terminal; if all agents reach a
             terminal location, then a state is terminal
@@ -240,7 +310,8 @@ class KolumboState(AbstractState):
         return len(self.nonterminal_agents) == 0
 
     @property
-    def possible_actions(self) -> list:
+    def possible_actions(self):
+        # type: () -> list
         """ The possible actions to take at this state
             Make sure that the returned list is not empty unless the state is
             a terminal state
@@ -249,11 +320,13 @@ class KolumboState(AbstractState):
         start_loc = self._statuses[self._agent_id][0]
         return [KolumboAction(self._agent_id, start_loc, path[1],
                               self.cost_at_path(*path))
-                for path in self.outgoing_paths(start_loc)]
+                for path in self.outgoing_paths(start_loc)
+                if self.cost_at_path(*path) <= self._time_remains]
 
-    def execute_action(self, action: KolumboAction) -> "KolumboState":
+    def execute_action(self, action):
+        # type: (KolumboAction) -> KolumboState
         """ Execute the action on a copy of the current state
-        :param action: The action io take
+        :param action: The action to take
         :return: A copy of the state after the action is executed
         """
         new_state = self.__copy__()
@@ -263,10 +336,12 @@ class KolumboState(AbstractState):
         new_state.evolve()
         return new_state
 
-    def visualize(self) -> Figure:
+    def visualize(self):
+        # type: () -> Figure
         raise NotImplementedError("Visualization method not implemented")
 
-    def input_from_ros(self, whatever_args) -> "KolumboState":
+    def input_from_ros(self, whatever_args):
+        # type: () -> KolumboState
         raise NotImplementedError("ROS interface not implemented")
 
 
@@ -275,4 +350,5 @@ if __name__ == '__main__':
     env.add_node(1, reward=1.0)
     env.add_node(2, reward=2.0)
     env.add_edge(1, 2, cost=2.4)
-    print(env.out_edges([1], data=True))
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    # nx.draw(env)
